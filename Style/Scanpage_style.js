b1e34526ad94b1f0226fb8b2a1dbef874f0393de
Egzavyer <xavier.lermusieaux@gmail.com>
2023-10-24 19:55:07 -0400

    link au modèle tensorflow.js
    ne marche pas à cause d'erreurs de dependency

--- Style/Scanpage_style.js
+++ Style/Scanpage_style.js
@@ -1,36 +1,36 @@
 import { StyleSheet } from "react-native";
 
 const ScanStyle = StyleSheet.create({
-    Scanning : {
-        justifyContent:"center",
-        alignItems: "center",
-        marginTop : 150
-    },
-    ScanText : {
-        fontFamily : "pBold",
-        fontSize: 30,
-        color: '#7AA8AE'
-    },
-    CamArea : {
-        backgroundColor: "#d6d6d6",
-        margin : "auto",
-        width: 350,
-        left: '7%',
-        height: 350,
-        margin: 'auto',
-        marginTop : 50,
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    cam_icon :{
-        width :50,
-        height : 50
-    },
-    Darkmode :{
-        backgroundColor: "#231f26"
-    },
-    Lightmode :{
-        backgroundColor: "#ffffff"
-    }
-})
+	Scanning: {
+		justifyContent: "center",
+		alignItems: "center",
+		marginTop: 150,
+	},
+	ScanText: {
+		fontFamily: "pBold",
+		fontSize: 30,
+		color: "#7AA8AE",
+	},
+	CamArea: {
+		backgroundColor: "#d6d6d6",
+		margin: "auto",
+		width: 350,
+		left: "7%",
+		height: 350,
+		margin: "auto",
+		marginTop: 50,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	cam_icon: {
+		width: 50,
+		height: 50,
+	},
+	Darkmode: {
+		backgroundColor: "#231f26",
+	},
+	Lightmode: {
+		backgroundColor: "#ffffff",
+	},
+});
 export default ScanStyle;
--- app/Scanpage.js
+++ app/Scanpage.js
@@ -1,29 +1,75 @@
-import {Text, SafeAreaView} from "react-native";
+import { Text, SafeAreaView } from "react-native";
 import layout_styles from "../Style/Layoutstyle.js";
 import { Image, Pressable, Appearance, useColorScheme } from "react-native";
 import ScanStyle from "../Style/Scanpage_style.js";
 import styles from "../Style/Homepage_style.js";
-import * as cam_image from '../src/image/homepage_image.png';
+import * as cam_image from "../src/image/homepage_image.png";
 import { Link } from "expo-router";
 
+import {
+	getModel,
+	convertBase64ToTensor,
+	startPrediction,
+} from "../helpers/tensorflow-helper.js";
+import { cropPicture } from "../helpers/image-helper.js";
+import { Camera } from "expo-camera";
 
-const Scan = ()=>{
-    let colorsheme = useColorScheme();
-    const ContainerTheme = colorsheme ==='light'? ScanStyle.Lightmode : ScanStyle.Darkmode;
-    return (
-        <SafeAreaView>
-            <SafeAreaView style ={ScanStyle.Scanning}>
-                <Text style = {ScanStyle.ScanText}>Scanning</Text>
-            </SafeAreaView>
-            <SafeAreaView style={ScanStyle.CamArea}>
-            <Image source ={require('../src/image/camera_icon.png')} style={ScanStyle.cam_icon}></Image>
-            </SafeAreaView>
-            <Link href="/Resultpage" style = {styles.Pressable} asChild>
-            <Pressable style = {styles.Pressable}>
-                    <Text style={styles.Button}>Scan</Text>
-            </Pressable>
-            </Link>
-        </SafeAreaView>
-    )
-}
+const RESULT_MAPPING = ["Positive COVID Test", "Negative COVID Test"];
+
+const Scan = () => {
+	const cameraRef = useRef();
+	const [isProcessing, setIsProcessing] = useState(false);
+	const [result, setResult] = useState("");
+
+	const handleImageCapture = async () => {
+		setIsProcessing(true);
+		const imageData = await cameraRef.current.takePictureAsync({
+			base64: true,
+		});
+		processImagePrediction(imageData);
+	};
+
+	const processImagePrediction = async (base64Image) => {
+		const croppedDate = await cropPicture(base64Image, 300);
+		const model = await getModel();
+		const tensor = await convertBase64ToTensor(croppedDate.base64);
+
+		const prediction = await startPrediction(model, tensor);
+
+		const highestPrediction = prediction.indexOf(
+			Math.max.apply(null, prediction)
+		);
+		setResult(RESULT_MAPPING[highestPrediction]);
+	};
+
+	let colorsheme = useColorScheme();
+	const ContainerTheme =
+		colorsheme === "light" ? ScanStyle.Lightmode : ScanStyle.Darkmode;
+	return (
+		<SafeAreaView>
+			<SafeAreaView style={ScanStyle.Scanning}>
+				<Text style={ScanStyle.ScanText}>Scanning</Text>
+			</SafeAreaView>
+			<SafeAreaView style={ScanStyle.CamArea}>
+				<Camera
+					ref={cameraRef}
+					style={ScanStyle.Cam}
+					type={Camera.Constants.Type.back}
+					autoFocus={true}
+					whiteBalance={Camera.Constants.WhiteBalance.auto}
+				/>
+			</SafeAreaView>
+			<Link href='/Resultpage' style={styles.Pressable} asChild>
+				{" "}
+				//make so that if result is positive, it will go to positive result page
+				//and if result is negative, it will go to negative result page on press
+				//of rescan, it will, setIsProcessing(false) and setResult("")
+				<Pressable style={styles.Pressable}>
+					onPress={() => handleImageCapture()}
+					<Text style={styles.Button}>Scan</Text>
+				</Pressable>
+			</Link>
+		</SafeAreaView>
+	);
+};
 export default Scan;
