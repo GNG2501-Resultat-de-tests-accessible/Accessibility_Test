528be55ad9ec5d164790be75b53aff7844fc5666
Yousssefch <youssefchakir003@gmail.com>
2023-10-26 21:44:04 -0400

    Homepage Animation
    added some animation to the homepage

--- app/Homepage.js
+++ app/Homepage.js
@@ -3,7 +3,7 @@ import styles from "../Style/Homepage_style.js";
 import layout_styles from "../Style/Layoutstyle.js";
 import { Image, Button, Pressable, Appearance, useColorScheme, StatusBar } from "react-native";
 import { Link } from "expo-router";
-import {SlideInDown,FadeInUp } from "react-native-reanimated";
+import {SlideInDown,FadeInUp, FadeInDown } from "react-native-reanimated";
 import Animated from "react-native-reanimated";
 
 const Home = () =>{
@@ -17,9 +17,9 @@ const Home = () =>{
             <StatusBar  backgroundColor= {colorsheme=== 'light'? "fffff": "#231f26"}/>
             <Animated.Text style={styles.Title} entering={FadeInUp}>Test Access</Animated.Text>                                                                               
             <SafeAreaView style ={layout_styles.Center}>                
-                <Text style={WelcomTextTheme}>Welcome</Text> 
-                <Text style={IndicationTextTheme}>Click on Start to begin scanning</Text>
-                <Image source ={require('../src/image/homepage_image.png')} style={styles.Imagee}></Image>
+                <Animated.Text entering={FadeInUp} style={WelcomTextTheme}>Welcome</Animated.Text> 
+                <Animated.Text entering={FadeInUp} style={IndicationTextTheme}>Click on Start to begin scanning</Animated.Text>
+                <Animated.Image entering={FadeInDown} source ={require('../src/image/homepage_image.png')} style={styles.Imagee}></Animated.Image>
             </SafeAreaView>
             <Link href="/Scanpage" style = {styles.Pressable} asChild>
             <Pressable >
