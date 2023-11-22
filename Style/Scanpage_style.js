48167808a4d9c673fd32e4103e49a9993dd195ff
Yousssefch <youssefchakir003@gmail.com>
2023-11-22 18:10:20 -0500

    conflict merge resolved

--- Style/Scanpage_style.js
+++ Style/Scanpage_style.js
@@ -4,24 +4,6 @@ const ScanStyle = StyleSheet.create({
     Scanning : {
         justifyContent:"center",
         alignItems: "center",
-<<<<<<< Updated upstream
-        marginTop : 50
-    },
-    ScanText : {
-        fontFamily : "pBold",
-        fontSize: 25,
-        color: '#7AA8AE'
-    },
-    CamArea : {
-        flex : 1,
-        margin : "auto",
-        width: 350,
-        left: '7%',
-        height: 10,
-        borderRadius : 10,
-        margin: 'auto',
-        marginTop : 80,
-=======
         marginTop : 30
     },
     ScanText : {
@@ -36,7 +18,6 @@ const ScanStyle = StyleSheet.create({
         borderRadius : 10,
         margin: 'auto',
         marginTop : 25,
->>>>>>> Stashed changes
         justifyContent: "center",
         alignSelf: "center",
         height : "80%", //Bottom margin for camera area
@@ -47,13 +28,9 @@ const ScanStyle = StyleSheet.create({
     },
     Darkmode :{
         backgroundColor: "#231f26",
-<<<<<<< Updated upstream
-        height : "100 %"
-=======
         height : "120%",
         position : "absolute",
         width : "100%"
->>>>>>> Stashed changes
     },
     Lightmode :{
         backgroundColor: "#ffffff",
@@ -68,26 +45,15 @@ const ScanStyle = StyleSheet.create({
     ContainingBox : {
         backgroundColor : "#151317",
         position :"absolute",
-<<<<<<< Updated upstream
-        width : "100 %",
-        height : "100%",
-        marginTop: 180,
-        borderRadius : 50,
-=======
         width : "100%",
         height : "100%",
         marginTop: 150,
         borderRadius : 30, //Rounded Corners For Background
->>>>>>> Stashed changes
     },
     CameraStyle : {
         flex :1,
         width : "100%",
-<<<<<<< Updated upstream
-        maxHeight:450
-=======
         maxHeight : "100%" 
->>>>>>> Stashed changes
 
 
     }
